f310e9b66a347761f000360b3e915c36d91979f4
Douglas Shirilla <douglasshirilla@yahoo.com>
2017-04-05 10:06:44 -0400

    Create Junit test class for bank class

---
+++ Project_6/BankTest.java
@@ -0,0 +1,69 @@
+package Project_6;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BankTest {
+
+	@Test
+	public void testBank() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddAccount() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemoveAccount() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetAccounts() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetAccount() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddCustomer() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemoveCustomer() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetCustomers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetCustomerString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetCustomerStringString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetCustomersAccounts() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
