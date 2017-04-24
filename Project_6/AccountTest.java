29412fe746f358010438334ae4990ba429ace733
Douglas Linkhart <dlinkhart@hawkmail.hccfl.edu>
2017-04-23 22:12:59 -0400

    Added Account Tests

--- Project_6/AccountTest.java
+++ Project_6/AccountTest.java
@@ -1,46 +1,183 @@
 package Project_6;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
 
 import java.util.List;
-import java.util.SortedSet;
+
+import org.junit.Before;
+import org.junit.Test;
+
+//import java.util.List;
+//import java.util.SortedSet;
 
 public class AccountTest {
+	
+	Bank testBankObj;
+	
+	Customer customer;
+	
+	SavingsAccount savingsAccount;
+	
+	final String bankName = "Test Bank";
+	final String lastName = "Jones";
+	final String firstName = "Tom";
+	final String description = "Test description";
+	
+	final double initialBalance = 123.45;
+	
+	@Before
+	public void init() {
+				
+		// Create Bank object
+		testBankObj = new Bank(bankName);
+		
+		// Create Customer Object
+		customer = new Customer(testBankObj, lastName, firstName);
+		
+		// Create SavingsAccount
+		savingsAccount = new SavingsAccount(customer, initialBalance, 
+				                            description);
+	}
 
+	// Test Account constructor
 	@Test
 	public void testAccount() {
-		fail("Not yet implemented");
-//		Account ( Customer cust, double initBal, String desc )
-	}
 		
+		// True if account was created
+		assertNotNull(savingsAccount);
+		
+		// True if balance is correct
+		assertTrue(savingsAccount.getBalance() == initialBalance);
+		
+		// True if description is correct
+		assertEquals(savingsAccount.getAccountDescription(), description);
+	}
+	
+	// Test deposit
 	@Test
 	public void testDeposit() { 
-		fail("Not yet implemented");
-//		abstract void deposit (double amount); 
+
+		double newBalance = 2 * initialBalance;
+		
+		/* Deposit an amount equal to the initial balance.
+		 * This should double the balance, so the new balance should 
+		 * equal newBalance.
+		 */
+		savingsAccount.deposit(initialBalance);
+		
+		// True if the balance is correct
+		assertTrue(savingsAccount.getBalance() == newBalance);
 	}
 	
+	// Test withdraw
 	@Test
 	public void testWithdraw() {	
-		fail("Not yet implemented");
-//		abstract void withdraw (double amount); 
+		
+		double newBalance = initialBalance - 1.00;
+
+		/* Withdraw one dollar.
+		 * This should reduce the balance by one dollar.
+		 */
+		savingsAccount.withdraw(initialBalance);
+		
+		// True if balance is correct
+		assertTrue(savingsAccount.getBalance() == newBalance);
 	}
 	
+	// Test transfer
 	@Test
 	public void testTransfer() {
-		fail("Not yet implemented");
-//		static void transfer ( Account fromAccount, Account toAccount, double amount )  
-	}
+
+		double newBalance  = initialBalance - 1.00;
+		double newBalance2 = initialBalance + 1.00;
+		
+		// Create a second account
+		SavingsAccount savingsAccount2 = new SavingsAccount(customer, 
+				                                            initialBalance, 
+                                                            description);
+		
+		/* Transfer one dollar from savingsAccount to savingsAccount2
+		 * This should reduce the balance of savingsAccount by one dollar
+		 * and increase the balance of savingsAccount2 by one dollar.
+		 */
+		Account.transfer(savingsAccount, savingsAccount2, 1.00);
+		
+		// True if balance is correct
+		assertTrue(savingsAccount.getBalance() == newBalance);
+		
+		// True if balance is correct
+		assertTrue(savingsAccount2.getBalance() == newBalance2);
+	} // testTransfer
 	
+	// Test getTransactions
 	@Test
 	public void	testGetTransactions() {
-		fail("Not yet implemented");
-//		List<Transaction> getTransactions ()  
-	}
-	
+		
+		double depositAmount = 1.00;
+		
+		/* Perform a transaction so the transaction list will hold at least 
+		 * one transaction
+		 */
+		savingsAccount.deposit(depositAmount);
+		
+		// Create the transaction list
+		List<Transaction> transactionList = savingsAccount.getTransactions();
+		
+		// True if list exists
+		assertNotNull(transactionList);
+		
+		// If the list exists...
+		if (transactionList != null) {
+			
+			// Get the transaction from the list
+			Transaction transaction = transactionList.get(0);
+			
+			// True if transaction type is correct
+			assertEquals(transaction.getType(), TransactionType.DEPOSIT);
+			
+			// True if transaction amount is correct
+			assertTrue(transaction.getAmount() == depositAmount);			
+		} // if list not null		
+	} // testGetTransactions
+
+	// Test getTransaction
 	@Test
 	public void testGetTransaction() {	
-		fail("Not yet implemented");
-//		Transaction getTransaction ( int transactionId ) { 
-	}
+
+		double depositAmount = 1.00;
+		
+		/* Perform a transaction so the transaction list will hold at least 
+		 * one transaction
+		 */
+		savingsAccount.deposit(depositAmount);
+		
+		// Create the transaction list
+		List<Transaction> transactionList = savingsAccount.getTransactions();
+		
+		// If the list exists...
+		if (transactionList != null) {
+			
+			// Get the transaction from the list
+			Transaction transaction = transactionList.get(0);
+			
+			// Get the transaction ID
+			int transactionID = transaction.getId();
+			
+			// Get transaction from transaction ID
+			Transaction transactionFromID = savingsAccount.getTransaction(
+					                                       transactionID);
+			
+			// True if transaction is returned
+			assertNotNull(transactionFromID);
+			
+			// True if transaction type is correct
+			assertEquals(transactionFromID.getType(), TransactionType.DEPOSIT);
+			
+			// True if transaction amount is correct
+			assertTrue(transactionFromID.getAmount() == depositAmount);
+		} // if list not null
+		else { // Else transaction list does not exist
+			fail("Transaction list does not exist");
+		}
+	} // testGetTransaction
 } // Class AccountTest
